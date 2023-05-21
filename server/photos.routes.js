908839adf7723034a7d3cabdf4058e04878844ec
Hilari <hilari@example.com>
2023-05-21 13:50:35 -0500

    First commit

---
+++ server/photos.routes.js
@@ -0,0 +1,29 @@
+const {Router} = require('express')
+
+const {uploadFile, readFile} = require('./s3')
+
+const router = Router()
+
+router.get('/', (req, res) => res.send('Welcome to server'))
+
+router.post('/upload', async(req, res) => {
+
+    console.log(req.files['photo'])
+    const result = await uploadFile(req.files['photo'])
+
+    console.log(result)
+
+    res.send('Archivo subido')
+})
+
+router.get('/archivo/:fileName', async(req, res) => {
+    try{
+        const result = await readFile(req.params.fileName)
+        res.send('Archivo descargado')
+
+    }catch (error) {
+        res.send(error.message)
+    }
+})
+
+module.exports = router
